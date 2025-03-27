8a006cf878ecc4681dea13e85537f2db11f177c1
wayne <wayne.pang@accenture.com>
2025-03-27 14:48:52 +0800

    Fixed double router issue

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from "react-router";
 import './index.css'
 import App from './App.tsx'
+import { StrictMode } from 'react'
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+  <StrictMode>
     <App />
-  </BrowserRouter>,
+  </StrictMode>,
 )

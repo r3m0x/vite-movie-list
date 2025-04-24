ea8e655dcd83319cb7d19c77d266e33b83a4e4e2
Wayne <waynepangxxx@gmail.com>
2025-04-24 12:09:32 +0800

    Add tanstack query support for later use

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { RouterProvider } from '@tanstack/react-router'
+import { router } from './route/Router'
 import './index.css'
-import { StrictMode } from 'react'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './route/Router.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
+const queryClient = new QueryClient()
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </React.StrictMode>,
 )

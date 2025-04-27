d7905fc27537a31e76ac1abd0b574a95f531ab90
Wayne <wayne.pang@accenture.com>
2025-04-28 00:03:01 +0800

    Fix updateBooking api service

--- src/services/user.tsx
+++ src/services/user.tsx
@@ -6,7 +6,10 @@ export const userApi = {
   getMoviesList: () => api.get("api/getMoviesList"),
   getMyBookings: () => api.get("api/getMyBookings"),
   addBooking: (ticket: Ticket) => api.post(`/api/addBooking/`, ticket),
-  updateBooking: (ticket: Ticket) => api.post("api/updateBooking", ticket),
+  updateBooking: (ticket: Ticket) =>
+    api.post("api/updateBooking", {
+      ...ticket,
+    }),
   deleteBooking: (ticketId: string) =>
     api.delete("api/deleteBooking", {
       params: {
